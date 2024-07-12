edf601882a1358da2d2b5818f5c64300fa0c049e
Dicky <dicky.dian1@gmail.com>
2024-07-12 22:54:39 +0700

    Try to refresh 404 not found

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import {Suspense} from 'react'
-import {Outlet} from 'react-router-dom'
-import {I18nProvider} from '../_metronic/i18n/i18nProvider'
-import {LayoutProvider, LayoutSplashScreen} from '../_metronic/layout/core'
-import {MasterInit} from '../_metronic/layout/MasterInit'
-import {AuthInit} from './modules/auth'
-import {ThemeModeProvider} from '../_metronic/partials'
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import { I18nProvider } from "../_metronic/i18n/i18nProvider";
+import { LayoutProvider, LayoutSplashScreen } from "../_metronic/layout/core";
+import { MasterInit } from "../_metronic/layout/MasterInit";
+import { AuthInit } from "./modules/auth";
+import { ThemeModeProvider } from "../_metronic/partials";
 
 const App = () => {
   return (
@@ -20,7 +20,7 @@ const App = () => {
         </LayoutProvider>
       </I18nProvider>
     </Suspense>
-  )
-}
+  );
+};
 
-export {App}
+export { App };
